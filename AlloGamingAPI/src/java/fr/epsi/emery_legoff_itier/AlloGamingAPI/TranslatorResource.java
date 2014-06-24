f91235e2033342e8b537eb5e6f9c385e561d9dfd
cedricEmery <cedric.emery34@gmail.com>
2014-06-24 14:33:03 +0200

    It WORK !!!!!
    http://localhost:8080/AlloGamingAPI/webresources/translator/GetPlatformsList
    
    http://localhost:8080/AlloGamingAPI/webresources/translator/GetGamesList?name=halo

--- AlloGamingAPI/src/java/fr/epsi/emery_legoff_itier/AlloGamingAPI/TranslatorResource.java
+++ AlloGamingAPI/src/java/fr/epsi/emery_legoff_itier/AlloGamingAPI/TranslatorResource.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-package services;
+package fr.epsi.emery_legoff_itier.AlloGamingAPI;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
@@ -12,79 +12,89 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
-import javax.ejb.Stateless;
-import javax.jws.WebMethod;
 import javax.jws.WebParam;
-import javax.jws.WebService;
-import javax.jws.soap.SOAPBinding;
-import javax.jws.soap.SOAPBinding.Style;
-import static javax.swing.text.html.FormSubmitEvent.MethodType.GET;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
 import org.apache.commons.io.IOUtils;
 import org.json.XML;
 
-
 /**
+ * REST Web Service
  *
- * @author Cedric
+ * @author Fixe Cedric
  */
-@WebService()
-@SOAPBinding(style = Style.RPC)
-//@javax.ws.rs.ApplicationPath("translator")
-//@Path("translator")
-public class translator {
+@Path("translator")
+public class TranslatorResource {
+
+    @Context
+    private UriInfo context;
 
     /**
-     * This is a sample web service operation
+     * Creates a new instance of TranslatorResource
      */
-    //@GET
-    //@WebMethod(operationName = "GetGamesList")
+    public TranslatorResource() {
+    }
+
+    
+    @GET
+    @Produces("text/plain")
+    @Path("GetGamesList")
     public String GetGamesList(@WebParam(name = "gameName") String sGameName) throws MalformedURLException, UnsupportedEncodingException 
     {
         URL url = new URL("http://thegamesdb.net/api/GetGamesList.php?name=halo"+ URLEncoder.encode(sGameName,"UTF-8"));
         return getXMLfromJson(url);
     }
 
-    //@GET
-    //@WebMethod(operationName = "GetGame")
+    @GET
+    @Produces("text/plain")
+    @Path("GetGame")
     public String GetGame(@WebParam(name = "gameId") long lGameId) throws MalformedURLException 
     {
         URL url = new URL("http://thegamesdb.net/api/GetGame.php?id="+ lGameId);
         return getXMLfromJson(url);
     }
 
-    //@GET
-    //@WebMethod(operationName = "GetArt")
+    @GET
+    @Produces("text/plain")
+    @Path("GetArt")
     public String GetArt(@WebParam(name = "artId") long lArtId) throws MalformedURLException 
     {
         return getXMLfromJson(new URL("http://thegamesdb.net/api/GetArt.php?id="+ lArtId));
     }
 
-    //@GET
-    //@Path("GetPlatformsList")
-    @WebMethod(operationName = "GetPlatformsList")
+    @GET
+    @Produces("text/plain")
+    @Path("GetPlatformsList")
     public String GetPlatformsList() throws MalformedURLException 
     {
         return getXMLfromJson(new URL("http://thegamesdb.net/api/GetPlatformsList.php"));
     }
     
-    //@GET
-    //@WebMethod(operationName = "GetPlatform")
+    @GET
+    @Produces("text/plain")
+    @Path("GetPlatform")
     public String GetPlatform(@WebParam(name = "platformId") long lPlatformId) throws MalformedURLException 
     {
         return getXMLfromJson(new URL("http://thegamesdb.net/api/GetPlatform.php?id="+ lPlatformId));
     }
     
-    //@GET
-    //@WebMethod(operationName = "getPlatformGames")
+    @GET
+    @Produces("text/plain")
+    @Path("GetPlatformGames")
     public String GetPlatformGames(@WebParam(name = "platformId") long lPlatformId) throws MalformedURLException 
     {
         return getXMLfromJson(new URL("http://thegamesdb.net/api/GetPlatformGames.php?platform="+ lPlatformId));
     }
     
-    //@GET
-    //@WebMethod(operationName = "PlatformGames")
+    @GET
+    @Produces("text/plain")
+    @Path("PlatformGames")
     public String PlatformGames(@WebParam(name = "platformName") String sPlatformName) throws UnsupportedEncodingException, MalformedURLException 
     {
         return getXMLfromJson(new URL("http://thegamesdb.net/api/PlatformGames.php?platform="+ URLEncoder.encode(sPlatformName,"UTF-8")));
